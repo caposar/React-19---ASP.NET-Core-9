1e8bf5a8b21f01a3147be3b2bbbf2abca9275731
Cesar <chpvhr@gmail.com>
2025-11-13 12:03:08 -0300

    Ruteo (completado)

--- react-peliculas/src/componentes/Boton.tsx
+++ react-peliculas/src/componentes/Boton.tsx
@@ -1,7 +1,7 @@
 export default function Boton(props: BotonProps){
 
     return (
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-primary" onClick={props.onClick}>
             {props.children}
         </button>
     )
@@ -10,4 +10,5 @@ export default function Boton(props: BotonProps){
 
 interface BotonProps{
     children: React.ReactNode;
+    onClick(): void;
 }
